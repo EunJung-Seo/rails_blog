0a1f34c536280910cf880acd51720332d609bedf
EunJung-Seo <soudg2@gmail.com>
2016-05-24 17:36:15 +0900

    Add invalid attribute factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -7,7 +7,15 @@ FactoryGirl.define do
     name 'user1'
   end
 
-  factory :invalid_post, parent: :post do
+  factory :invalid_post_title, parent: :post do
     title nil
+  end
+
+  factory :short_post_title, parent: :post do
+    title '5'
+  end
+
+  factory :invalid_post_name, parent: :post do
+    name nil
   end
 end
