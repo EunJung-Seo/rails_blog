1fa4531141a1fcb9d29087b334eee81de0c6ecc9
EunJung-Seo <soudg2@gmail.com>
2016-05-31 11:07:56 +0900

    Remove find_post_by_id method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  private
-
-  def find_post_by_id
-    @post = Post.find_by_id(params[:id])
-    if @post.blank?
-      respond_to do |format|
-        format.html { redirect_to posts_url }
-        format.json { render json: { error: 'Post not found' }, status: :unprocessable_entity }
-      end
-    end
-  end
 end
