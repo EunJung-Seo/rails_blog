e16fb6b6cc9921ae9cb7813fdca18bc058df41a9
EunJung-Seo <soudg2@gmail.com>
2016-05-31 11:25:35 +0900

    Create find_post_by_id method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,33 @@
 # Comment Controller
 class CommentsController < ApplicationController
+  before_filter :find_post_by_id
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  # ===============================================================
+  #
+  #                         PRIVATE
+  #
+  # ===============================================================
+
+  private
+
+  def find_post_by_id
+    @post = Post.find_by_id(params[:post_id])
+    if @post.blank?
+      respond_to do |format|
+        format.html { redirect_to posts_url }
+        format.json { render json: { error: 'Post not found' }, status: :unprocessable_entity }
+      end
+    end
   end
 end
