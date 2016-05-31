f304277e0739db9d0defe2cf6d9e2d2d85357dd4
EunJung-Seo <soudg2@gmail.com>
2016-05-31 16:32:26 +0900

    Create find_comment_by_id method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 # Comment Controller
 class CommentsController < ApplicationController
   before_filter :find_post_by_id
+  before_filter :find_comment_by_id, only: [:destroy]
 
   def create
     @comment = @post.comments.create(params[:comment])
@@ -8,7 +9,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
@@ -27,6 +27,16 @@ class CommentsController < ApplicationController
       respond_to do |format|
         format.html { redirect_to posts_url }
         format.json { render json: { error: 'Post not found' }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def find_comment_by_id
+    @comment = @post.comments.find(params[:id])
+    if @comment.blank?
+      respond_to do |format|
+        format.html { redirect_to request.referer }
+        format.json { render json: { error: 'Comment not found' }, status: :unprocessable_entity }
       end
     end
   end
