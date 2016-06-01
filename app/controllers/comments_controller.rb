44ff898951a89d7ebcff9946795ac5a495e1d83f
EunJung-Seo <soudg2@gmail.com>
2016-06-01 18:35:49 +0900

    Create before_filter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,29 +25,20 @@ class CommentsController < ApplicationController
   def find_post_by_id
     @post = Post.find_by_id(params[:post_id])
     if @post.blank?
-      respond_to do |format|
-        format.html { redirect_to posts_url, flash[:alert] => '존재하지 않는 포스트입니다.' }
-        format.json { render json: { error: 'Post not found' }, status: :unprocessable_entity }
-      end
+      redirect_to posts_url, alert: '존재하지 않는 포스트입니다.'
     end
   end
 
   def find_comment_by_id
-    @comment = @post.comments.find(params[:id])
+    @comment = @post.comments.find_by_id(params[:id])
     if @comment.blank?
-      respond_to do |format|
-        format.html { redirect_to request.referer, flash[:alert] => '존재하지 않는 코멘트입니다.' }
-        format.json { render json: { error: 'Comment not found' }, status: :unprocessable_entity }
-      end
+      redirect_to post_path(@post), alert: '존재하지 않는 코멘트입니다.'
     end
   end
 
   def check_comment_validation
     unless Comment.new(params[:comment]).valid?
-      respond_to do |format|
-        format.html { redirect_to request.referer, flash[:alert] => '유효하지 않은 코멘트입니다.' }
-        format.json { render json: { error: 'Invalid comment' }, status: :unprocessable_entity }
-      end
+      redirect_to post_path(@post), alert: '유효하지 않은 코멘트입니다.'
     end
   end
 end
