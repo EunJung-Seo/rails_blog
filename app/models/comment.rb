198f183463ecd20b3c203a07513cb863cc5c108b
EunJung-Seo <soudg2@gmail.com>
2016-06-02 17:20:15 +0900

    Add next line

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,6 @@ class Comment < ActiveRecord::Base
 
   validates :commenter, presence: true,
                         format: { with: /\A\w+\z/, message: '특수문자는 사용할 수 없습니다.'}
-  validates :body, presence: true, length: { :maximum => 32 }
+  validates :body, presence: true,
+                        length: { :maximum => 32 }
 end
